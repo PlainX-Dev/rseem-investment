1874f063e691433861a139b8f2604d0f7a3fb991
PlainX <computer4you1977@gmail.com>
2026-04-04 16:02:14 +0200

    fix: remove conflicting i18n config for App Router

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // RTL Support
-  i18n: {
-    locales: ['en', 'ar'],
-    defaultLocale: 'en',
-  },
 };
 
 module.exports = withNextIntl(nextConfig);
